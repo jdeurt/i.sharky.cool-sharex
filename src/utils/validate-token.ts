f39a83383511bb0a1e62cc969d51688cd3b9691b
Juan de Urtubey <jdeurt@gmail.com>
2019-03-09 01:17:56 -0600

    Moved token to headers.

--- src/utils/validate-token.ts
+++ src/utils/validate-token.ts
@@ -6,12 +6,12 @@ export default function(req: Request, res: Response, next: NextFunction) {
         info: "TOKEN env var is not set."
     });
 
-    if (!req.body.token) return res.status(400).json({
+    if (!req.headers["S-Token"]) return res.status(400).json({
         success: false,
-        info: "Missing 'token' field in request body."
+        info: "Missing 'S-Token' header."
     });
 
-    if (req.body.token != process.env.TOKEN) return res.status(403).json({
+    if (req.headers["S-Token"] != process.env.TOKEN) return res.status(403).json({
         success: false,
         info: "Invalid token provided."
     });
