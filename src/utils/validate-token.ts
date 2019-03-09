b6d9025f427b44087c0a8eeb171cc171397905b5
Juan de Urtubey <jdeurt@gmail.com>
2019-03-09 01:20:56 -0600

    Token in query?

--- src/utils/validate-token.ts
+++ src/utils/validate-token.ts
@@ -6,12 +6,12 @@ export default function(req: Request, res: Response, next: NextFunction) {
         info: "TOKEN env var is not set."
     });
 
-    if (!req.headers["S-Token"]) return res.status(400).json({
+    if (!req.query.token) return res.status(400).json({
         success: false,
         info: "Missing 'S-Token' header."
     });
 
-    if (req.headers["S-Token"] != process.env.TOKEN) return res.status(403).json({
+    if (req.query.token != process.env.TOKEN) return res.status(403).json({
         success: false,
         info: "Invalid token provided."
     });
