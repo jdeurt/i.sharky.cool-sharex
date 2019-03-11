94033734a2699ad7a9c92c7dfced6c23727bdc1d
Juan de Urtubey <jdeurt@gmail.com>
2019-03-11 13:49:31 -0500

    Added creating the views path first lol.

--- build-scripts/copy-views.js
+++ build-scripts/copy-views.js
@@ -4,6 +4,10 @@ const path = require("path");
 const viewsPath = path.resolve(__dirname, "../src/views");
 const outputPath = path.resolve(__dirname, "../dist/views");
 
+if (!fs.existsSync(outputPath)) {
+    fs.mkdirSync(outputPath);
+}
+
 const viewsDir = fs.readdirSync(viewsPath);
 
 viewsDir.forEach(file => {
