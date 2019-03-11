5cba0b85251878bfff70cdef060abe8c4d872039
Juan de Urtubey <jdeurt@gmail.com>
2019-03-11 13:48:10 -0500

    Fixed typo.

--- build-scripts/copy-views.js
+++ build-scripts/copy-views.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
-const viewsPath = path.resolve(__dirname + "../src/views");
-const outputPath = path.resolve(__dirname + "../dist/views");
+const viewsPath = path.resolve(__dirname, "../src/views");
+const outputPath = path.resolve(__dirname, "../dist/views");
 
 const viewsDir = fs.readdirSync(viewsPath);
 
